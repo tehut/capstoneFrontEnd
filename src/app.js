a992f66a5445d1d26b4af0374369b860299e9882
Tehut Getahun <tehut.m.g@gmail.com>
2017-07-26 10:35:51 -0700

    removed login functionality

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@
 
 // Import jQuery & Underscore
 import $ from 'jquery';
-import _ from 'underscore';
+var _ = require('lodash');
 import StudentListView from 'views/student_list_view';
 import StudentList from 'collections/student_list';
+
 var Cookies = require('js-cookie');
+
 // import LoginView from 'views/login.js'
 
 
@@ -13,40 +15,7 @@ var studentList = new StudentList();
 
 // ready to go
 $(document).ready(function() {
-//
-$('#login').click(function(event){
-  // event.preventDefault;
-  var  email = $('#inputEmail').val();
-  var password = $('#inputPassword').val();
-  var hash =  btoa(email + ':' + password);
-  var tok =  hash;
-  //
-  // console.log(tok);
-  Cookies.set('token', tok);
-  console.log(Cookies.get('token'));
 
-})
-
-    var options = {
-      el: $('#foundation'),
-      model: 'Student',
-      method: 'GET',
-      template: _.template($("#site-template").html()),
-    };
-  // });
-
-    //
-    // var studentListDisplay = new StudentListView(options);
-    //  studentListDisplay.render();
-
-
-
-
-  // $('#order-form').hide();
-
-  // $('#library').click(function() {
-  // studentList.fetch();
-  // });
 
 });
 //
@@ -55,11 +24,65 @@ function loadQuickbooks($el) {
 }
 
 function loadForms($el) {
+  var stringHtml="";
+  stringHtml += "<section id='site'>";
+  stringHtml += "  <header class= \"row 13\">";
+  stringHtml += "    <h3 id='page-title' class=\"columns large-4 medium-4 small-12\" >";
+  stringHtml += "      Student Information";
+  stringHtml += "    <\/h3>";
+  stringHtml += "        <hr>";
+  stringHtml += "  <\/header>";
+  stringHtml += "  <main>";
+  stringHtml += "";
+  stringHtml += "    <section class=\" columns large-4 small-12\">";
+  stringHtml += "      <form class=\"form-horizontal\">";
+  stringHtml += "        <\/div>";
+  stringHtml += "        <div>";
+  stringHtml += "            <div class=\"control-group\">";
+  stringHtml += "              <label> Choose Classroom";
+  stringHtml += "                <select>";
+  stringHtml += "                  <option value=633> South Campus <\/option>";
+  stringHtml += "                  <option value=634> North: Ladybug <\/option>";
+  stringHtml += "                  <option value=635>North: Butterly <\/option>";
+  stringHtml += "                <\/select>";
+  stringHtml += "              <\/label>";
+  stringHtml += "            <\/div>";
+  stringHtml += "        <\/div>";
+  stringHtml += "        <div>";
+  stringHtml += "            <div class=\"control-group\">";
+  stringHtml += "              <label> Choose Operation";
+  stringHtml += "                <select>";
+  stringHtml += "                  <option value=633> Download Applicant Age List <\/option>";
+  stringHtml += "                  <option value=634> Download Potty Training Report <\/option>";
+  stringHtml += "                  <option value=635>Download Class Birthday List <\/option>";
+  stringHtml += "                <\/select>";
+  stringHtml += "              <\/label>";
+  stringHtml += "            <\/div>";
+  stringHtml += "        <\/div>";
+  stringHtml += "      <\/form>";
+  stringHtml += "";
+  stringHtml += "      <button type=\"submit\" class=\"button woogs\" id=\"search\">Download Class Email List<\/button>";
+  stringHtml += "      <button type=\"submit\" class=\"button\" id=\"library\">Download Applicant Age List<\/button>";
+  stringHtml += "";
+  stringHtml += "    <\/div>";
+  stringHtml += "";
+  stringHtml += "  <\/section>";
+  stringHtml += "";
+  stringHtml += "";
+  stringHtml += "<\/section >";
+  stringHtml += "";
+  $('#foundation').html(stringHtml);
+  var options = {
+    el: $('#foundation'),
+    model: 'Student',
+    method: 'GET',
+    template: _.template($("#site-template").html()),
+  };
+  var preloadSite =  _.template($("#site-template").html());
+  var studentListDisplay = new StudentListView(options);
+  studentListDisplay.render();
 
-      var studentListDisplay = new StudentListView(options);
-       studentListDisplay.render();
-  // $el.html('<h1>Let us print forms</h1>')
-  // $.getScript('https://wmsfront.herokuapp.com/build/app.bundle.js', function(){console.log("TG: successfully loaded script")})
+
 }
 
 console.log(
@@ -79,7 +102,7 @@ $(function() {
   tc.Plugins.menuControl.primary.add('Print Forms', tc.utils.schoolUrl('networks/wedgwood/school/forms'), 'print')
 
   if (document.URL.endsWith('/networks/wedgwood/school/quickbooks')) {
-    loadQuickbooks($('#foundation'))
+loadQuickbooks($('#foundation'))
   } else if (document.URL.endsWith('/networks/wedgwood/school/forms')) {
     loadForms($('#foundation'))
   }
